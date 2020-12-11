402cdd18c4efcd25b20edccbcd55ec3586ba9157
GCaptainNemo <1136003685@qq.com>
2020-12-12 01:22:41 +0800

    add myfilte, mymedian

--- src/average_median_filter.m
+++ src/average_median_filter.m
@@ -1,34 +1,32 @@
 grid = imread('../resources/add_noise.jpg');
 grid = rgb2gray(grid);
-% grid = imnoise(grid,'salt & pepper',0.02);
-grid = imnoise(grid,'gaussian');
+grid = imnoise(grid,'salt & pepper',0.02);
+% grid = imnoise(grid,'gaussian');
 I_3 = temp('average',[3, 3]);%3*3均值滤波
 I_5 = temp('average',[5, 5]);%3*3均值滤波
 I_7 = temp('average',[7, 7]);%3*3均值滤波
 
+% med_3 = medfilt2(grid, [3, 3]);
+med_3 = mymedian(grid, [3, 3]);
+med_5 = mymedian(grid, [5, 5]);
+med_7 = mymedian(grid, [7, 7]);
+average_3 = myfilt(grid, I_3);
+average_5 = myfilt(grid, I_5);
+average_7 = myfilt(grid, I_7);
 
-med_3 = medfilt2(grid, [3, 3]);
-med_5 = medfilt2(grid, [5, 5]);
-med_7 = medfilt2(grid, [7, 7]);
-average_3 = imfilter(grid, I_3);
-average_5 = imfilter(grid, I_5);
-average_7 = imfilter(grid, I_7);
 
 figure(1);
 subplot(141);  imshow(grid); title('原图像');
-
 subplot(142);  imshow(average_3); title('3x3均值滤波');
 subplot(143);  imshow(average_5); title('5x5均值滤波');
 subplot(144);  imshow(average_7); title('7x7均值滤波');
 
 figure(2);
 subplot(141);  imshow(grid); title('原图像');
-
 subplot(142);  imshow(med_3); title('3x3中值滤波');
 subplot(143);  imshow(med_5); title('5x5中值滤波');
 subplot(144);  imshow(med_7); title('7x7中值滤波');
-% average_5 = imfilter(grid, I_5);
-% average_7 = imfilter(grid, I_7);
+
 
 function template = temp(text, size)
 if strcmp(text, 'average')
@@ -37,9 +35,39 @@ if strcmp(text, 'average')
 end
 end
 
+function out = myfilt(img, temp)
+[temprow, tempcolumn] = size(temp);
+[row, column] = size(img);
+out = zeros(size(img));
+padd_row = floor(temprow/ 2); padd_column = floor(tempcolumn / 2);
+img = [zeros(row, padd_column), img, zeros(row, padd_column)];
+img = [zeros(padd_row, column + 2*padd_column); img; zeros(padd_row, column + 2*padd_column)];
+img = double(img);
+for row_ = padd_row+1:padd_row + row 
+    for column_ = padd_column+1:padd_column + column
+       out(row_ - padd_row, column_ - padd_column) = sum(sum(img(row_-padd_row:row_+padd_row, ...
+       column_-padd_column:column_+padd_column) .* temp));
+    end
+end
+out = uint8(out);
+end
 
-
-
+function out = mymedian(img, temp)
+temprow = temp(1); tempcolumn = temp(2);
+[row, column] = size(img);
+out = zeros(size(img));
+padd_row = floor(temprow/ 2); padd_column = floor(tempcolumn / 2);
+img = [zeros(row, padd_column), img, zeros(row, padd_column)];
+img = [zeros(padd_row, column + 2*padd_column); img; zeros(padd_row, column + 2*padd_column)];
+img = double(img);
+for row_ = padd_row+1:padd_row + row 
+    for column_ = padd_column+1:padd_column + column
+       out(row_ - padd_row, column_ - padd_column) = median(reshape(img(row_-padd_row:row_+padd_row, ...
+       column_-padd_column:column_+padd_column),temprow * tempcolumn, 1));
+    end
+end
+out = uint8(out);
+end
 
 
 
--- src/edge_detection_operator.m
+++ src/edge_detection_operator.m
@@ -35,11 +35,11 @@ end
 function Img_kir = kirsch(Img)
 
 %提取图象边缘
-t=[0.8 1.0 1.5 2.0 2.5].*10^5 ;     %设定阈值
+t = 250000; %设定阈值
 Img5 = double(Img);            
 [m,n] = size(Img5);             
-g=zeros(m,n); 
-d=zeros(1,8);
+g = zeros(m,n); 
+d = zeros(1,8);
 %利用Kirsch算子进行边缘提取
 for i=2:m-1
    for j=2:n-1
@@ -56,14 +56,13 @@ for i=2:m-1
 end
 
 %显示边缘提取后的图象
-for k=1:5
-    for i=1:m
-        for j=1:n
-            if g(i,j)>t(k)
-                Img_kir(i,j) = 255;           
-            else
-                Img_kir(i,j) = 0;
-            end
+
+for i=1:m
+    for j=1:n
+        if g(i,j)>t
+            Img_kir(i,j) = 255;           
+        else
+            Img_kir(i,j) = 0;
         end
     end
 end
